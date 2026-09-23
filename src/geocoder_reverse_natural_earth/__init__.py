b82cf1c113f05a529951626e6ffda74464b25e9b
Heiko Klein <heiko.klein@met.no>
2026-09-23 14:37:57 +0200

    new dev-version, __version__

--- src/geocoder_reverse_natural_earth/__init__.py
+++ src/geocoder_reverse_natural_earth/__init__.py
@@ -1,1 +1,5 @@
 from .geocoder_reverse_ne import Geocoder_Reverse_NE, Geocoder_Reverse_Exception
+
+# set __version__ from the package metadata
+from importlib.metadata import version
+__version__ = version("geocoder_reverse_natural_earth")
